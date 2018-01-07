65f976d6f045cf851997314ccdeeed493dfec80f
Lisennk <serewann@gmail.com>
2018-01-07 22:02:29 +0200

    start developing builder

--- docs/status.js
+++ docs/status.js
@@ -1,6 +1,6 @@
 (function() {
 
-    var platform = 'http://176.57.214.62:8800';
+    var platform = 'http://127.0.0.1:8892';
     $.getJSON(platform + "/stats").done(function(data) {
         $(miners).text(data.miners);
         $(connections).text(data.connections);
